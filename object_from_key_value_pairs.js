0a919519baf25b82dc8e0e7ff75f7e10913e49b0
S Biswas <ab.sarfaraj@gmail.com>
2023-03-20 23:00:17 +0530

    key pairs arr to object

---
+++ object_from_key_value_pairs.js
@@ -0,0 +1,13 @@
+
+const keyValuePairsArr = [
+    ["key1", "Java Script"],
+    ["key2", "Type Scripts"],
+    ["key3", "Object"],
+    ["key4", "Programming"],
+];
+
+console.log(keyValuePairsArr);
+
+const obj = Object.fromEntries(keyValuePairsArr);
+
+console.log(obj);
