fdc4e04a9bcd0ffca86fcccbf25f02263845b49e
S Biswas <ab.sarfaraj@gmail.com>
2023-03-22 18:37:13 +0530

    let, var & const

---
+++ var_let_const.js
@@ -0,0 +1,21 @@
+//var, let and const 
+
+function showName(){
+    var name = "Sarfaraj";
+    let address = "Kolkata";
+
+    console.log("name : " + name);
+    console.log("address : " + address);
+
+    if(true){
+        let phone = "8017665566";
+
+        console.log("phone : " + phone);
+    }
+    console.log("phone : " + phone);
+
+}
+
+showName();
+
+// console.log("name : " + name);
