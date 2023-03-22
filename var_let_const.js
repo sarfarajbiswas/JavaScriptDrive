9bbfbcea14e0f0e9198174f581060580888e33a3
S Biswas <ab.sarfaraj@gmail.com>
2023-03-22 19:08:21 +0530

    arrow func

--- var_let_const.js
+++ var_let_const.js
@@ -18,12 +18,30 @@ function showName(){
     // console.log("phone : " + phone);
 }
 
+calc(40);
+
 function calc(a, b=10){
     console.log(`Calculation: ${a+b}`);
 }
 
+
 showName();
 
-calc(40);
 
-// console.log("name : " + name);
+//fat arrow function
+
+const sum = ()=>{
+    let num1 = 10;
+    let num2 = 30;
+
+    const sumVal = num1 + num2;
+
+    console.log(`Sum: ${sumVal}`);
+}
+
+sum();
+
+//single line statement
+const sumArrow = ()=> `Sum: ${10 + 90}`;
+
+console.log(sumArrow());
